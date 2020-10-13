76f4d45c223c1825f45c5cce24fb6abbc8309ccf
Tobi Lehman <mail@tobilehman.com>
2020-10-12 21:41:05 -0700

    Add crib assets

---
+++ content/crib/precache-manifest.0ffefaf62cb35ec4a28e62518cd1ded7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1605e4fca5590a19dd8328102ffd83a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9bbd0db02487a7c31b3",
+    "url": "/crib/static/css/main.7e519544.chunk.css"
+  },
+  {
+    "revision": "28645aeee611ee1b30d2",
+    "url": "/crib/static/js/2.2d27a602.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crib/static/js/2.2d27a602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9bbd0db02487a7c31b3",
+    "url": "/crib/static/js/main.2e25a47d.chunk.js"
+  },
+  {
+    "revision": "372de703bd208ba48cfb",
+    "url": "/crib/static/js/runtime-main.7eacacdc.js"
+  }
+]);
