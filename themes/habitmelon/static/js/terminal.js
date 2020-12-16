a9660178f6ce400684b9ea887ee17a0f86d5a9e8
Tobi Lehman <mail@tobilehman.com>
2020-12-15 20:42:41 -0800

    get the terminal repl working

--- themes/habitmelon/static/js/terminal.js
+++ themes/habitmelon/static/js/terminal.js
@@ -1,18 +1,47 @@
+function terminalOnClick(event) {
+    document.getElementById("command").focus();
+    console.log("focusing invisible textarea");
+}
+
+function command(target) {
+    const input = target.value;
+    const key = input[input.length-1];
+    // If user hits Enter
+    if(key === "\n") {
+        console.log("Enter");
+        // clear input
+        target.value = " ";
+        // copy terminal-line
+        let promptNode = document.getElementById("prompt").cloneNode(true);
+        let commandVisible = document.getElementById("commandVisible").cloneNode(false);
+        let blinkNode = document.getElementById("blink").cloneNode(true);
+        document.getElementById("commandVisible").removeAttribute("id");
+        commandVisible.innerText += " ";
+        document.getElementById("prompt").removeAttribute("id");
+        document.getElementById("blink").remove();
+        let newLine = document.createElement("div", {"class": "terminal-line"});
+        newLine.appendChild(promptNode);
+        newLine.appendChild(commandVisible);
+        newLine.appendChild(blinkNode);
+        document.getElementById("terminal-lines").appendChild(newLine);
+        // font-size: 20px, scroll to y-position lines.count * font-size
+        let terminal = document.getElementById("terminal");
+        document.getElementById("terminal").scroll(0, terminal.scrollHeight);
+    } else {
+        document.getElementById("commandVisible").innerText = target.value;
+    }
+}
 
 const commands = {
     "uname": "TobiOS"
 };
+
 window.onkeyup = function (event) {
     const cmd = document.getElementById("command");
     const key = event.key;
     const text = cmd.innerText;
-    if(key === "Backspace") {
-        cmd.innerText = text.substring(0, text.length-1);
-    } else if(key === "Enter") {
+    if (key === "Enter") {
         cmd.innerHTML += "<br />";
         cmd.innerText += title;
-    } else if(key.length === 1) {
-        cmd.innerText += key;
-    }
-
+    } 
 }
