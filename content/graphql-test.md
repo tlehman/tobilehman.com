7a87c7c32718124fff81a424f07517aba43919ea
Tobi Lehman <mail@tobilehman.com>
2020-12-21 08:42:07 -0800

    Setup webpack and NPM

---
+++ content/graphql-test.md
@@ -0,0 +1,13 @@
+---
+title: "GraphQL Test"
+date: 2020-12-21T08:31:18-08:00
+---
+
+<div class="app-body">
+    <button id="MutationEventButton">Add data</button>
+    <div id="MutationResult"></div>
+    <div id="QueryResult"></div>
+    <div id="SubscriptionResult"></div>
+</div>
+
+<script src="src/main.bundle.js"></script>
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'static/src')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
