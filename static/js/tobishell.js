7947c01c7e943d22e283024447008dac4f88cf77
Tobi Lehman <mail@tobilehman.com>
2021-11-21 12:32:07 -0800

    Add help cmd and refactor handler code

--- static/js/tobishell.js
+++ static/js/tobishell.js
@@ -23,17 +23,10 @@ const fs = {
     "/home/tobi/about": [".", ".."],
     "/home/tobi/archive": [".", "..", "hgttg.mp3"]
 };
-function prompt() {
-    term.write("\r\n");
-    curr_line = curr_line.trim();
-    const { groups: { cmd, args } } = /^(?<cmd>[a-z]+) *(?<args>.*)$/.exec(curr_line);
-
-    switch(cmd) {
-        case "back":
-            window.history.back();
-            break;
-        case "cd":
-            if(args == "..") {
+const commands = [
+    { name: "back", handler: () => window.history.back() },
+    { name: "cd", handler: (args) => {
+            if (args == "..") {
                 var href = window.location.href;
                 var hrefParts = href.split("/");
                 hrefParts.reverse();
@@ -42,87 +35,115 @@ function prompt() {
                 hrefParts.reverse();
                 window.location.href = hrefParts.join("/");
             }
-            else if(args == "~" || args == "/home/tobi") {
+            else if (args == "~" || args == "/home/tobi") {
                 window.location.href = "/";
             } else {
                 window.location.href = args;
             }
-            break;
-        case "clear":
+        }
+    },
+    { name: "clear", handler: () => {
             entries.push(curr_line);
             term.clear();
             curr_line = "";
             term.write(PS1() + curr_line);
-            break;
-        case "ls":
-            const path = args.startsWith("-") || args == "" ? pwd : args;
-            const items = fs[path];
-            // ls -1 shows each item on new line
-            if(curr_line.match(/\-1/)) {
-                delimiter = "\r\n";
-            } else {
-                delimiter = "\t";
-            }
-            // If there are items in the "filesystem". Otherwise just spoof it
-            if(!items && curr_line.match(/\-a/)) {
-                term.write(".");
+        }
+    },
+    { name: "ls", handler: (args) => {
+        const path = args.startsWith("-") || args == "" ? pwd : args;
+        const items = fs[path];
+        // ls -1 shows each item on new line
+        if(curr_line.match(/\-1/)) {
+            delimiter = "\r\n";
+        } else {
+            delimiter = "\t";
+        }
+        // If there are items in the "filesystem". Otherwise just spoof it
+        if(!items && curr_line.match(/\-a/)) {
+            term.write(".");
+            term.write(delimiter);
+            term.write("..");
+        }
+        // Assuming there are items in the "filesystem"
+        for(var i = 0; items && i < items.length; i++) {
+            if(!items[i].startsWith(".")) {
+                term.write(items[i]);
                 term.write(delimiter);
-                term.write("..");
-            }
-            // Assuming there are items in the "filesystem"
-            for(var i = 0; items && i < items.length; i++) {
-                if(!items[i].startsWith(".")) {
+            } else {
+                if(curr_line.match(/\-a/)) {
                     term.write(items[i]);
                     term.write(delimiter);
-                } else {
-                    if(curr_line.match(/\-a/)) {
-                        term.write(items[i]);
-                        term.write(delimiter);
-                    }
                 }
             }
-            // Special case, if path == "posts", then show the blog posts:
-            if(path === "/home/tobi/posts") {
-                // Display all hugo-generated listItems (see habitmelon/layouts/_default/list.html)
-                for(var i = 0; i < listItems.length; i++) {
-                    if(listItems[i].split("/").length >= 3) {
-                        term.write(listItems[i].split("/")[2]);
-                        term.write(delimiter);
-                    }
+        }
+        // Special case, if path == "posts", then show the blog posts:
+        if(path === "/home/tobi/posts") {
+            // Display all hugo-generated listItems (see habitmelon/layouts/_default/list.html)
+            for(var i = 0; i < listItems.length; i++) {
+                if(listItems[i].split("/").length >= 3) {
+                    term.write(listItems[i].split("/")[2]);
+                    term.write(delimiter);
                 }
             }
+        }
 
-            entries.push(curr_line);
-            curr_line = "";
-            term.write("\r\n");
-            term.write(PS1() + curr_line);
-            break;
-        case "pwd":
+        entries.push(curr_line);
+        curr_line = "";
+        term.write("\r\n");
+        term.write(PS1() + curr_line);
+    }},
+    { name: "pwd", handler: () => {
             term.write(pwd);
             curr_line = "";
             term.write("\r\n");
             term.write(PS1() + curr_line);
-            break;
-        case "mail":
-            curr_line = "";
-            term.write("mail@tobilehman.com\r\n");
-            term.write(PS1() + curr_line);
-            break;
-        case "uname":
-            term.write("TobiOS");
-            if(args == "-a") {
-                term.write(" 33.5");
+        }
+    },
+    
+    { name: "mail", handler: () => {
+        curr_line = "";
+        term.write("mail@tobilehman.com\r\n");
+        term.write(PS1() + curr_line);
+    } },
+    { name: "uname", handler: (args) => {
+        term.write("TobiOS");
+        if(args == "-a") {
+            term.write(" 33.5");
+        }
+        curr_line = "";
+        term.write("\r\n");
+        term.write(PS1() + curr_line);
+    }}
+];
+function prompt() {
+    term.write("\r\n");
+    curr_line = curr_line.trim();
+    const { groups: { cmd, args } } = /^(?<cmd>[a-z]+) *(?<args>.*)$/.exec(curr_line);
+
+    if(cmd == "help") {
+        term.write("Here are all the commands you can type: ");
+        term.write("\r\n");
+        commands.forEach((command, i) => {
+            term.write("  " + command.name);
+            if((i+1) % 8 == 0) {
+                term.write("\r\n  ");
+            } else {
+                term.write(" ");
             }
-            curr_line = "";
-            term.write("\r\n");
-            term.write(PS1() + curr_line);
-            break;
-        default:
-            term.write("tobish: command not found: " + curr_line);
-            curr_line = "";
-            term.write("\r\n");
-            term.write(PS1() + curr_line);
-            break;
+        });
+        term.write("\r\n");
+        term.write(PS1());
+        return;
+    }
+
+    const command_handler = commands.filter((command) => command.name == cmd)[0]?.handler;
+    if(command_handler) {
+        command_handler(args);
+    } else {
+        term.write("tobish: command not found: " + curr_line);
+        curr_line = "";
+        term.write("\r\n");
+        term.write(PS1() + curr_line);
     }
 }
 
